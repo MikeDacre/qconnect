f77b84a9b44cfed7726a6a9ae411c6b706934ac8
Mike Dacre <mike.dacre@gmail.com>
2014-10-10 11:20:37 -0700

    User jobid and be more robust
    xpra should auto disconnect in the event of qdel now, also I am using
    the pbs jobid instead of the uid to prevent errors

--- qguiconnect.sh
+++ qguiconnect.sh
@@ -7,12 +7,13 @@
 #
 
 
-qstat=$(qstat -n -1 $1) 
+qstat=$(qstat -n -1 $1 | tail -n 1) 
 ret=$?
 if [[ ! $ret == '0' ]]; then
   exit $ret
 fi
-node=$(echo $qstat | tail -n 1 | sed 's/.* //g' | sed 's#/.*##g')
+node=$(echo $qstat | sed 's/.* //g' | sed 's#/.*##g')
+job=$(echo $qstat | sed 's/ .*//g' | sed 's#\..*##g')
 
 echo "Connecting to GUI..."
 echo "NOTE: To disconnect without terminating you MUST kill"
@@ -20,4 +21,4 @@ echo "this job from the command line"
 echo "Closing the window directly will terminate the program"
 sleep 2
 
-xpra attach ssh:$USER@$node:$UID
+xpra attach ssh:$USER@$node:$job

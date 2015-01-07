0757acbc4467873db9eb5a821239e0be4dabdef9
Mike Dacre <mike.dacre@gmail.com>
2015-01-07 15:22:53 -0800

    Updated makepkg program

--- packages/make_package.sh
+++ packages/make_package.sh
@@ -5,17 +5,31 @@
 # Distributed under terms of the MIT license.
 #
 
+if [[ $# < 1 ]]; then
+  echo "Please provide version number"
+  exit 1
+fi
+
 pkgver=$1
 pkgname=qconnect
 pkgfiles=(qconnect.py qconnect.1.gz README.md LICENSE)
 
+echo "Creating ${pkgname}_${pkgver}"
 mkdir ${pkgname}_${pkgver}
 for i in $pkgfiles; do
   cp ../$i ${pkgname}_${pkgver}
 done
 
+echo "Creating tarball"
 tar czf ${pkgname}_${pkgver}.tar.gz ${pkgname}_${pkgver}
 
 rm -rf ${pkgname}_${pkgver}
 
+echo "Signing"
 gpg --detach-sign ${pkgname}_${pkgver}.tar.gz
+
+echo "Creating symlinks"
+rm ${pkgname}_latest.tar.gz
+rm ${pkgname}_latest.tar.gz.sig
+ln -s ${pkgname}_${pkgver}.tar.gz ${pkgname}_latest.tar.gz
+ln -s ${pkgname}_${pkgver}.tar.gz.sig ${pkgname}_latest.tar.gz.sig
